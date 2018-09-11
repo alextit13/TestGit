93f2451011ba897efcb630298b17a6c200180d31
Alexey <alexcas@mail.ru>
2018-09-11 22:11:09 +0300

    Two

--- app/src/main/java/com/audiobooks/android/testgenerics/StartClass.java
+++ app/src/main/java/com/audiobooks/android/testgenerics/StartClass.java
@@ -10,5 +10,6 @@ public class StartClass extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // first
+        // two
     }
 }
